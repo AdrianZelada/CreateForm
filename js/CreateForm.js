6013a9cff6ae5775cd215052956d54d887d60452
Zelada Adrian <zelada_torrez@hotmail.com>
2015-02-22 00:36:10 -0400

    createForm update

--- js/CreateForm.js
+++ js/CreateForm.js
@@ -2,6 +2,10 @@
 var CreateForm = function(ObjectConfig){			
 	ObjectConfig.forEach(function(item){
         //mandar los objetos con la funcion Bind .... enviandole como en el video xD
+
+        var mask=NewObject.mak.bind(item);
+
+        // agregar los objetos al container ..... add(mask);
 	});	
 };
 
@@ -10,7 +14,7 @@ var NewObject={
 	type:'text',
 	label:'Label 1',
     _id:'_1',
-    mak:function(container){
+    mak:function(){
         var NewContent=document.createElement('div');
         var NewLabel=document.createElement('p');
         var NewInput=document.createElement('input').type(this.type).id(this._id);
@@ -18,7 +22,6 @@ var NewObject={
 
        //formar aca los elementos
 
-        container.appendChild();
     }
 
 }
