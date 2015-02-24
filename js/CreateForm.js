4933e282305d9e3a78a02f0ece6df6ac24ac261b
Zelada Adrian <zelada_torrez@hotmail.com>
2015-02-24 16:40:59 -0400

    upload createForm

--- js/CreateForm.js
+++ js/CreateForm.js
@@ -1,10 +1,8 @@
 'use strik';
 var CreateForm = function(ObjectConfig){    
 
-
     var Content = document.getElementById(ObjectConfig.Content);
     var ObjectForm=ObjectConfig.Forms;
-
 
 	ObjectConfig.Forms.forEach(function(item){
         //mandar los objetos con la funcion Bind .... enviandole como en el video xD
@@ -20,49 +18,49 @@ var CreateForm = function(ObjectConfig){
 };
 
 var NewObject=function(ObjectF){
-    console.log(ObjectF.type);
 	type=ObjectF.type ? ObjectF.type : 'text' ;
 	label=ObjectF.label ?  ObjectF.label : 'Label 1';
     _id=ObjectF._id ? ObjectF._id : '_1';
     options=ObjectF.options ? ObjectF.options : [];
     group_title=ObjectF.group_title ? ObjectF.group_title : 'title group';
+    name_group=ObjectF.name_group ? ObjectF.name_group : 'group1';
     this.mak=function(){
         var NewContent=document.createElement('div');
-        //var NewLabel=document.createElement('p');
-        //NewLabel.textContent=this.label;
-        if(type=="Group_radio"){
-            console.log("group");
-            var resutl=this.mak_Radio();
-
+        var NewLabel=document.createElement('p');
+        NewLabel.textContent=label;
+        if(type=="radio" || type=="checkBox"){
+            var resutl=this.mak_RadioBox();
             NewContent.appendChild(resutl);
-            console.log(resutl);
         }else{
-
+            var NewInput=document.createElement('input');
+            NewInput.type=type;
+            NewInput.id=_id;
+            NewContent.appendChild(NewLabel);
+            NewContent.appendChild(NewInput);
         }
-        /*var NewInput=document.createElement('input');
-        NewInput.type=this.type;
-        NewInput.id=this._id;
-        NewContent.appendChild(NewLabel);
-        NewContent.appendChild(NewInput);*/
-        return NewContent; 
+        return NewContent;
     };
-    this.mak_Radio=function(){
-        console.log(this.options);
-        var ContentRadio=document.createElement('div');
+    this.mak_RadioBox=function(){
+        var ContentRadioBox=document.createElement('div');
         var labelGroup=document.createElement('p');
-        labelGroup.textContent=this.label;
-        ContentRadio.appendChild(labelGroup);
+        var handleEvent=(type!="radio") ? "onclick" : "onchange";
+        ContentRadioBox.dataName=name_group;
+        labelGroup.textContent=label;
+        ContentRadioBox.appendChild(labelGroup);
+
         options.forEach(function(item){
             var labelRadio=document.createElement('p');
             labelRadio.textContent=item.title;
             var NewOption=document.createElement('input');
-            NewOption.type='radio';
+            NewOption.type=type;
             NewOption.value=item.value;
-            NewOption.addEventListener('onclick',function(){alert('hola')});
-            ContentRadio.appendChild(labelRadio);
-            ContentRadio.appendChild(NewOption);
+            NewOption[handleEvent]=function(event){
+                alert('cambiado');
+            };
+            ContentRadioBox.appendChild(labelRadio);
+            ContentRadioBox.appendChild(NewOption);
         });
 
-        return ContentRadio;
+        return ContentRadioBox;
     }
 }
