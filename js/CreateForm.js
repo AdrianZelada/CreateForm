6fb5684931cf46eaf480d78384f8ddf792c5749c
Zelada Adrian <zelada_torrez@hotmail.com>
2015-03-31 01:23:17 -0400

    otra vez

--- js/CreateForm.js
+++ js/CreateForm.js
@@ -6,31 +6,33 @@ var CreateForm = function(ObjectConfig){
 
 	ObjectConfig.Forms.forEach(function(item){
         //mandar los objetos con la funcion Bind .... enviandole como en el video xD
-
-      // var mask=NewObject.mak.bind(item);
-       //Content.appendChild(mask());
-
         var obForm=new NewObject(item);
         Content.appendChild(obForm.mak());
-
-        // agregar los objetos al container ..... add(mask);
 	});
 };
 
 var NewObject=function(ObjectF){
+
 	type=ObjectF.type ? ObjectF.type : 'text' ;
 	label=ObjectF.label ?  ObjectF.label : 'Label 1';
     _id=ObjectF._id ? ObjectF._id : '_1';
     options=ObjectF.options ? ObjectF.options : [];
     group_title=ObjectF.group_title ? ObjectF.group_title : 'title group';
     name_group=ObjectF.name_group ? ObjectF.name_group : 'group1';
+    callback=ObjectF.callback ? ObjectF.callback : null;
+
+    Class=ObjectF.Class ? ObjectF.Class : 'CreateForm';
+
+
     this.mak=function(){
         var NewContent=document.createElement('div');
-        var NewLabel=document.createElement('p');
+            NewContent.className=Class;
+        console.log(NewContent.className);
+        var NewLabel=document.createElement('label');
         NewLabel.textContent=label;
         if(type=="radio" || type=="checkBox"){
-            var resutl=this.mak_RadioBox();
-            NewContent.appendChild(resutl);
+            var result=this.mak_RadioBox();
+            NewContent.appendChild(result);
         }else{
             var NewInput=document.createElement('input');
             NewInput.type=type;
@@ -40,16 +42,18 @@ var NewObject=function(ObjectF){
         }
         return NewContent;
     };
+
     this.mak_RadioBox=function(){
+
         var ContentRadioBox=document.createElement('div');
-        var labelGroup=document.createElement('p');
+        var labelGroup=document.createElement('label');
         var handleEvent=(type!="radio") ? "onclick" : "onchange";
         ContentRadioBox.dataName=name_group;
         labelGroup.textContent=label;
         ContentRadioBox.appendChild(labelGroup);
 
         options.forEach(function(item){
-            var labelRadio=document.createElement('p');
+            var labelRadio=document.createElement('label');
             labelRadio.textContent=item.title;
             var NewOption=document.createElement('input');
             NewOption.type=type;
